c4e20f6d655a17be57d6b4b83ffdb62df0340bb0
U Sen Wan <senwan96@gmail.com>
2023-03-10 22:07:12 +0600

    create an api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Resources\EmployeeResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Employee;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/employees',function(){
+    $employees = Employee::orderBy('last_name')->get();
+    return EmployeeResource::collection($employees);
 });
